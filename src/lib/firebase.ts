04d0041296c6a762a769c051f1850ed9789e18f4
soportefibex <soportefibex@gmail.com>
2025-11-26 21:09:06 +0000

    feat: Add .gitignore and use environment variables for Firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,22 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7uqdn3jb1X5Fr_2gj3MtmEx_yTu1N47s",
-    authDomain: "fraudevenezuela.firebaseapp.com",
-    projectId: "fraudevenezuela",
-    storageBucket: "fraudevenezuela.firebasestorage.app",
-    messagingSenderId: "29074609436",
-    appId: "1:29074609436:web:c4cf4bd63d4c3f1e3a96d9",
-    measurementId: "G-V3C1641PWN"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
