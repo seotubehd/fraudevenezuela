1b5f99fd6b1d3c89c8f73d8b3fc7f61638cd77c4
soportefibex <soportefibex@gmail.com>
2025-11-29 23:37:03 +0000

    Fix security vulnerabilities

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,9 @@
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 // --- Standard Client-Side Firebase Initialization ---
-// This configuration is safe to be exposed in the browser.
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
